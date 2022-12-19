ed7ded2c2ffb5a921fc819669a798ebe249c0221
Code Sandbox <hello@codesandbox.io>
2022-12-19 04:27:53 +0000

    Deployment from Codesandbox.

---
+++ static/js/main.31e50e83.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpcode-tikki"]=this["webpackJsonpcode-tikki"]||[]).push([[0],{12:function(e,t,c){},14:function(e,t,c){"use strict";c.r(t);var i=c(1),n=c(3),o=(c(12),c(0));function s(){return Object(o.jsxs)("div",{className:"App",children:[Object(o.jsx)("h1",{children:"Hello CodeSandbox"}),Object(o.jsx)("h2",{children:"Start editing to see some magic happen!"})]})}var r=document.getElementById("root");Object(n.createRoot)(r).render(Object(o.jsx)(i.StrictMode,{children:Object(o.jsx)(s,{})}))}},[[14,1,2]]]);
+//# sourceMappingURL=main.31e50e83.chunk.js.map
